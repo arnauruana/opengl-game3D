4c44101b382d1a6b59c709f8054f2ed467d425b5
Arnau Ruana <arnau.ruana.i@est.fib.upc.edu>
2020-05-10 18:50:56 +0200

    First approach of shooting

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+
+public class Fireball : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+
+    public float speed;
+
+    private float forceX;
+    private float forceY;
+    private float forceZ;
+
+    void Start()
+    {
+        this.forceX = 0f;
+        this.forceY = 0f;
+        this.forceZ = speed;
+    }
+
+    void FixedUpdate()
+    {
+        float x = this.forceX * Time.deltaTime;
+        float y = this.forceY * Time.deltaTime;
+        float z = this.forceZ * Time.deltaTime;
+
+        this.rigidBody.AddForce(x, y, z);
+    }
+}
