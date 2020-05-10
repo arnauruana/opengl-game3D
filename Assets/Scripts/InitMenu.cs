6148100b39cbdf3b46084605bb8a265b65d23257
Arnau Ruana <arnau.ruana.i@est.fib.upc.edu>
2020-05-10 20:45:48 +0200

    Add little fixes

--- Assets/Scripts/InitMenu.cs
+++ Assets/Scripts/InitMenu.cs
@@ -1,17 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+
 
 public class InitMenu : MonoBehaviour
 {
     public Button play;
+
     public GameObject mainmenu;
     public GameObject credits;
     public GameObject options;
     public GameObject controls;
     public GameObject surequit;
-    // Start is called before the first frame update
+
     void Start()
     {
         credits.SetActive(false);
@@ -19,17 +19,5 @@ public class InitMenu : MonoBehaviour
         controls.SetActive(false);
         surequit.SetActive(false);
         mainmenu.SetActive(true);
-       
-        //play.Select();
-        
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-
-    
 }
