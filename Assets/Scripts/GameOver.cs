62cef820efe1550c687a73b6d53a1cb01b6c380e
Arnau Ruana <arnau.ruana.i@est.fib.upc.edu>
2020-05-14 18:59:47 +0200

    Add GameOver Menu prefab

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameOver : MonoBehaviour
+{
+    public Button restart;
+    public Button exit;
+
+    public void restartPressed()
+    {
+        SceneManager.LoadScene("Level1");
+        // SceneManager.LoadScene("Level2");
+    }
+
+    public void exitPressed()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
