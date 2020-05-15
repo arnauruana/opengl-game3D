37ea5ce03ad999cc02781cd5689557e99a9ca576
Arnau Ruana <arnau.ruana.i@est.fib.upc.edu>
2020-05-15 20:15:07 +0200

    Add explosions to Fireballs

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -3,13 +3,20 @@
 
 public class Fireball : MonoBehaviour
 {
+    [Header("Effects")]
+    public GameObject explosion;
+    public GameObject fire;
+
+    [Header("Options")]
     public float speed;
     public float destroyDelay;
+
+    private Collision collision;
 
     void Start()
     {
         this.transform.Rotate(0, 180, 0);
-        this.destroyFireball();
+        this.destroyFireball(false);
     }
 
     void FixedUpdate()
@@ -17,8 +24,31 @@ public class Fireball : MonoBehaviour
         this.transform.Translate(speed * Vector3.right * Time.deltaTime);
     }
 
-    public void destroyFireball()
+    void OnCollisionEnter(Collision collision)
     {
-        Destroy(this.gameObject, destroyDelay);
+        this.collision = collision;
+
+        this.destroyFireball(true);
+    }
+
+    public void destroyFireball(bool now)
+    {
+        if (now) // we have collided
+        {
+            Instantiate(this.explosion, this.transform.position, this.transform.rotation);
+            
+            if (this.collision.collider.tag == "Terrain") // fire on grass
+            {
+                Instantiate(this.fire, this.transform.position, this.transform.rotation);
+                Destroy(this.fire);
+            }
+
+            Destroy(this.explosion);
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject, destroyDelay);
+        }
     }
 }
