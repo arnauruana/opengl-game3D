24193168fc0ef4f763dddd3f5c90d587aa81ea9f
Arnau Ruana <arnau.ruana.i@est.fib.upc.edu>
2020-05-16 18:11:59 +0200

    Start GodMode implementation

---
+++ Assets/Scripts/GodMode.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GodMode : MonoBehaviour
+{
+    [Header("Status")]
+    public bool enabled;
+
+    private static GodMode instance;
+
+    private void Awake()
+    {
+        if (GodMode.instance == null)
+        {
+            GodMode.instance = this;
+            GameObject.DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            GodMode.Destroy(this.gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        this.enabled = false;
+    }
+
+    public void enable()
+    {
+        this.enabled = true;
+    }
+
+    public void disable()
+    {
+        this.enabled = false;
+    }
+
+    public bool isEnabled()
+    {
+        return this.enabled;
+    }
+
+    public void toggle()
+    {
+        this.enabled = !this.enabled;
+    }
+}
