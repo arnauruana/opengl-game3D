30ca36ee4f3efe359226ee58ceeb5b3516009b7b
Arnau Ruana <arnau.ruana.i@est.fib.upc.edu>
2020-05-25 14:07:41 +0200

    Started enemy ship interaction

---
+++ Assets/Scripts/EnemyShipCollision.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class EnemyShipCollision : MonoBehaviour
+{
+    public Level1Controller level1Controller;
+    public GameObject explosionEffect;
+       
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag != "EnemyFire")
+        {
+            if (collider.tag == "AllyFireball") // destruccion contra nuestros disparos
+            {
+                this.explode();
+            }
+            else // choque contra nuestra nave
+            {
+                this.destroy();
+            }
+        }
+    }
+
+    public void explode()
+    {
+        GameObject explosion = Instantiate(this.explosionEffect, this.transform.position, this.transform.rotation) as GameObject;
+
+        Destroy(explosion, 8);
+        Destroy(this.transform.parent.gameObject);
+    }
+
+    public void destroy()
+    {
+        Destroy(this.transform.parent.gameObject);
+    }
+}
--- Assets/Scripts/ShipCollision.cs
+++ Assets/Scripts/ShipCollision.cs
@@ -3,44 +3,33 @@
 public class ShipCollision : MonoBehaviour
 {
     public Level1Controller level1Controller;
-    public PlayerController player;
     public HealthBar health;
-
-    public Cinemachine.CinemachineDollyCart cart;
-
     public GameObject explosionEffect;
-
-    public float restartDelay;
        
-
+    
     void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag != "AllyFireball")
         {
-            if (collision.collider.tag == "EnemyFire") // impacto con misil enemigo
-            {
-                this.health.Damage(20);
-            }
-            else // choque contra obstaculo
-            {
-                this.explode();
-                this.level1Controller.gameOver();
-            }
+            this.explode();
+            this.level1Controller.gameOver();
         }
     }
 
     void OnTriggerEnter(Collider collider)
     {
-        
         //tag cambiable para distinguir enemigos
         if (collider.tag == "EnemyFire")
         {
             this.health.Damage(20);
         }
+        else
+        {
+            this.explode();
+            this.level1Controller.gameOver();
+        }
     }
     
-
-   
     public void explode()
     {
         GameObject explosion = Instantiate(this.explosionEffect, this.transform.position, this.transform.rotation) as GameObject;
