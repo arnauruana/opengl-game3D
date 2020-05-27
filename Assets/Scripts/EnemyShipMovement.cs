e8eb56951b018fe093ade37de237545908b3c956
Arnau Ruana <arnau.ruana.i@est.fib.upc.edu>
2020-05-27 13:30:03 +0200

    All enemies movement implemented

--- Assets/Scripts/EnemyShipMovement.cs
+++ Assets/Scripts/EnemyShipMovement.cs
@@ -5,6 +5,13 @@ public class EnemyShipMovement : MonoBehaviour
 {
     public float speed;
 
+    private float rotation;
+
+    void Awake()
+    {
+        this.rotation = 0;
+    }
+
     void FixedUpdate()
     {
         if (this.gameObject.tag == "EnemySpaceShip")
@@ -15,6 +22,18 @@ public class EnemyShipMovement : MonoBehaviour
         else if (this.gameObject.tag == "EnemyTank")
         {
             this.transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+        }
+        
+        else if (this.gameObject.tag == "EnemyDroid")
+        {
+            if (this.rotation == 360)
+            {
+                this.rotation = 0;
+            }
+
+            this.rotation += 150 * Time.deltaTime;
+            this.gameObject.transform.rotation = Quaternion.Euler(0, 0, this.rotation);
+            this.gameObject.transform.Translate(0, speed * Time.deltaTime, -speed * Time.deltaTime);
         }
     }
 }
