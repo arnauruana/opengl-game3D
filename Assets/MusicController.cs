d7bf189998e1736ab8d0366b8def11ad8577e02f
victorbarroso1 <victor.barroso1@est.fib.upc.edu>
2020-05-27 21:07:16 +0200

    Music Menu can be muted

--- Assets/MusicController.cs
+++ Assets/MusicController.cs
@@ -7,15 +7,37 @@ public class MusicController : MonoBehaviour
 
     public AudioSource transition;
     public AudioSource menuMusic;
+    public bool mute;
+    public float lastVolume;
+    public float volume;
+    public ShowValue showValue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mute = false;
+        volume = lastVolume =  100;
+        showValue.textUpdateVolume(volume);
+    }
+    private void Awake()
+    {
+        GameObject.DontDestroyOnLoad(this.gameObject);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            mute = !mute;
+            changeVolume();
+        }
+    }
+
+    public void changeVolume(){
+        if (!mute) volume = lastVolume;
+        else volume = 0f;
+        transition.volume = 0.06f * (volume / 100);
+        menuMusic.volume = 0.15f * (volume / 100);
     }
 }
--- Assets/Scripts/ShowValue.cs
+++ Assets/Scripts/ShowValue.cs
@@ -7,43 +7,21 @@ using UnityEngine.UI;
 public class ShowValue : MonoBehaviour
 {
     public TextMeshProUGUI percentageText;
+    public MusicController musicController;
+
     
 
-    public AudioSource transition;
-    public AudioSource menuMusic;
-    public bool mute;
-    public float volume;
-
-    void Start()
-    {
-        mute = false;
-        volume = 100;
-        textUpdateVolume(volume);
-    }
+    
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.M) )
-        {
-            if (mute)
-            {
-                mute = false;
-                textUpdateVolume(volume);
-
-            }
-            else
-            {
-                mute = true;
-                textUpdateVolume(0);
-            }
-
-        }
+        
     }
     public void textUpdateVolume(float value)
     {
-        if (!mute) volume = value;
+        musicController.lastVolume = value;
         this.percentageText.text = value + "%";
-        transition.volume = 0.06f * (value/100);
-        menuMusic.volume = 0.15f * (value/100);
+        musicController.changeVolume();
+        
     }
 
     
