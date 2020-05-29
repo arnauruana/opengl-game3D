2ce340c508b7925ddb38314037f22e5d7da0b6be
Arnau Ruana <arnau.ruana.i@est.fib.upc.edu>
2020-05-29 14:00:48 +0200

    Coliisions on AllyShip finished

--- Assets/Scripts/ShipCollision.cs
+++ Assets/Scripts/ShipCollision.cs
@@ -6,18 +6,30 @@ public class ShipCollision : MonoBehaviour
 
     public HealthBar health;
     public GameObject explosionEffect;
+    public Rigidbody rigidbody;
        
-    
+    private GodMode godMode;
+
+    void Awake()
+    {
+        this.godMode = GameObject.FindObjectOfType<GodMode>();
+    }
+
+    void Update()
+    {
+        this.rigidbody.isKinematic = this.godMode.isEnabled();
+    }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.tag != "AllyFireball") this.health.Damage(100);
+        if (collision.collider.tag == "Terrain") this.health.Damage(100);
     }
 
     void OnTriggerEnter(Collider collider)
     {
         //tag cambiable para distinguir enemigos
         if (collider.tag == "EnemyFire") this.health.Damage(20);
+        else if (collider.tag == "Terrain") this.health.Damage(100);
         else this.health.Damage(100);
     }
     
